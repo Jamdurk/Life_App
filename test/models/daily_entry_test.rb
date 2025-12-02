f2444ebfbda7a408b7d02c201d6473622e4a1ab7
James <james.savona@outlook.com>
2025-12-02 14:56:21 +1100

    Daily entry basic test suite

--- test/models/daily_entry_test.rb
+++ test/models/daily_entry_test.rb
@@ -1,7 +1,30 @@
 require "test_helper"
 
 class DailyEntryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do 
+    @daily_entry = daily_entries(:one)
+  end
+
+  test "daily entry is valid with valid attributes" do
+    assert @daily_entry.valid?
+  end
+
+  test "daily entry is not valid without a date" do
+    @daily_entry.date = nil 
+    assert_not @daily_entry.valid?
+    assert_includes @daily_entry.errors[:date], "can't be blank"
+  end
+
+  test "daily entry is not valid without day content" do
+    @daily_entry.day_content = nil 
+    assert_not @daily_entry.valid?
+    assert_includes @daily_entry.errors[:day_content], "can't be blank"
+  end
+
+  test "daily entry is not valid without mood" do
+    @daily_entry.mood = nil 
+    assert_not @daily_entry.valid?
+    assert_includes @daily_entry.errors[:mood], "can't be blank"
+  end
+  
 end
