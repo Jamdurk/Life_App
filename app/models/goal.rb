b4c19060c0238a530435a9b6127b88fcb5cf88d7
James <james.savona@outlook.com>
2025-12-02 15:05:34 +1100

    removed the start or end dates, as goals may have neither of these depending on it

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,6 +1,5 @@
 class Goal < ApplicationRecord
     # validations ensure the data must be present. No length limit here similar to the daily entries as these can be long.
-    validates :goal_start,   presence: true
-    validates :goal_end,     presence: true 
+    # Also no dates, as our goals may be long term or not having a specific start and end date.
     validates :goal_content, presence: true
 end
