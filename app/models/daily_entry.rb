f1149d9b90dcd900ea01e6945727a1e9ff242b5c
James <james.savona@outlook.com>
2025-12-02 14:23:24 +1100

    added basic validations for input to have data e.g. dates and content

--- app/models/daily_entry.rb
+++ app/models/daily_entry.rb
@@ -1,2 +1,7 @@
 class DailyEntry < ApplicationRecord
+    # validations ensure the data must be present. No limit is given since the entries will be quite long most likely.
+    validates :date, presence: true 
+    validates :day_content, presence: true
+    validates :text, presence: true
+    validates :mood, presence: true
 end
