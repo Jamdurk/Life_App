f2a95f3e966ecdf2fc891415de4ad53005ba6fca
James <james.savona@outlook.com>
2025-12-02 14:34:11 +1100

    more validations for other models

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,2 +1,6 @@
 class Goal < ApplicationRecord
+    # validations ensure the data must be present. No length limit here similar to the daily entries as these can be long.
+    validates :goal_start,   presence: true
+    validates :goal_end,     presence: true 
+    validates :goal_content, presence: true
 end
--- app/models/hobby.rb
+++ app/models/hobby.rb
@@ -1,3 +1,6 @@
 class Hobby < ApplicationRecord
-    
+    # validations ensure the data must be present. 
+    validates :start_date, presence: true
+    validates :end_date, presence: true
+    validates :hobby_content, presence: true, length: { maximum: 150 }
 end
