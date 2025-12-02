57943fa70509fec39d11d8ce85989ecf70de0d63
James <james.savona@outlook.com>
2025-12-02 15:06:48 +1100

    Further basic test suites for goal/hobby model, and to reflect changed validations

--- test/models/goal_test.rb
+++ test/models/goal_test.rb
@@ -1,7 +1,27 @@
 require "test_helper"
 
 class GoalTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do 
+    @goal = goals(:one)
+  end
+
+  test "goal is valid with valid attributes" do 
+    @goal.goal_start = nil
+    assert @goal.valid?
+  end
+
+  test "goal is valid with a start date" do
+    @goal.goal_start = nil
+    assert @goal.valid?
+  end
+
+  test "goal is valid an end date" do
+    assert @goal.valid?
+  end
+
+  test "goal is not valid without content" do
+    @goal.goal_content = nil 
+    assert_not @goal.valid?
+    assert_includes @goal.errors[:goal_content], "can't be blank"
+  end
 end
--- test/models/hobby_test.rb
+++ test/models/hobby_test.rb
@@ -1,7 +1,34 @@
 require "test_helper"
 
 class HobbyTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do 
+    @hobby = hobbies(:one)
+  end
+
+  test "hobby is valid with valid attributes" do
+    assert @hobby.valid?
+  end
+
+  test "hobby is valid without an end date" do
+    @hobby.end_date = nil 
+    assert @hobby.valid?
+  end
+
+  test "hobby is not valid without a start date" do
+    @hobby.start_date = nil 
+    assert_not @hobby.valid?
+    assert_includes @hobby.errors[:start_date], "can't be blank"
+  end
+
+  test "hobby is not valid without content" do
+    @hobby.hobby_content = nil 
+    assert_not @hobby.valid?
+    assert_includes @hobby.errors[:hobby_content], "can't be blank"
+  end
+
+  test "hobby is not valid with content over 150 characters" do
+    @hobby.hobby_content = "a" * 151
+    assert_not @hobby.valid?
+    assert_includes @hobby.errors[:hobby_content], "is too long (maximum is 150 characters)"
+  end
 end
