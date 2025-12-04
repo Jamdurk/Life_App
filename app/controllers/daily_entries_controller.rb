3a9e2dbe25b17f5f17588ef1372808a9f596af24
James <james.savona@outlook.com>
2025-12-04 18:13:31 +1100

    Added create action and params for daily entry controller

--- app/controllers/daily_entries_controller.rb
+++ app/controllers/daily_entries_controller.rb
@@ -1,2 +1,23 @@
 class DailyEntriesController < ApplicationController
+
+    def home 
+        @daily_entry = DailyEntry.new
+        @daily_entries = DailyEntry.order(date: :desc).limit(10)
+    end
+
+    def create
+        @daily_entry = DailyEntry.new(daily_entry_params)
+        if @daily_entry.save
+            redirect_to home_path, notice: "Daily entry created successfully"
+        else
+            render :home, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def daily_entry_params
+        params.require(:daily_entry).permit(:date, :day_content, :mood)
+    end
+
 end
