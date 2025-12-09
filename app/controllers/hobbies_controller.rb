788dcbdc7edc6fce390bae3362b85fb15631cb93
James <james.savona@outlook.com>
2025-12-09 16:04:35 +1100

    Hobby controller basic code

--- app/controllers/hobbies_controller.rb
+++ app/controllers/hobbies_controller.rb
@@ -1,2 +1,22 @@
 class HobbiesController < ApplicationController
+
+    def hobbies_page
+        @hobby = Hobby.new
+    end
+
+    def create
+        @hobby = Hobby.new(hobby_params)
+
+        if @hobby.save
+            redirect_to hobbies_page_path, notice: "Hobby created successfully"
+        else
+            render :hobbies_page, status: :unprocessable_entity
+        end
+
+
+
+    private 
+
+    def hobby_params
+        params.require(:hobby).permit(:start_date, :end_date, :hobby_content)
 end
