0531d2cdb52681c2a5a1d013cdaa0d5e4397b4b8
James <james.savona@outlook.com>
2025-12-15 19:08:14 +1100

    Added validation for goal start to be present

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -2,4 +2,5 @@ class Goal < ApplicationRecord
     # validations ensure the data must be present. No length limit here similar to the daily entries as these can be long.
     # Also no dates, as our goals may be long term or not having a specific start and end date.
     validates :goal_content, presence: true
+    validates :goal_start, presence: true
 end
