3865ff9e2b555f19b0f7704443c3bbb1dfec46d0
James <james.savona@outlook.com>
2025-12-16 17:26:12 +1100

    Basic test suite for home page

--- test/controllers/daily_entries_controller_test.rb
+++ test/controllers/daily_entries_controller_test.rb
@@ -1,7 +1,32 @@
 require "test_helper"
 
 class DailyEntriesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should get home" do
+    get root_path
+    assert_response :success
+  end
+
+  test "should create daily entry" do
+    assert_difference("DailyEntry.count") do
+      post daily_entries_path, params: { daily_entry: { day_content: "Test day content", mood: 1 } }
+    end
+    assert_redirected_to daily_entries_path
+  end
+
+  test "should not create daily entry without day content" do
+    assert_no_difference("DailyEntry.count") do
+      post daily_entries_path, params: { daily_entry: { day_content: "",  mood: 1} }
+      assert_select "div#error_explanation",  "The form contains 1 error • Day content can't be blank"
+    end
+  end
+
+  test "should not create daily entry without a mood" do
+    assert_no_difference("DailyEntry.count") do
+      post daily_entries_path, params: { daily_entry: { day_content: "Hi",  mood: nil } }
+      assert_select "div#error_explanation",  "The form contains 1 error • Mood can't be blank"
+    end
+  end
+    
+
 end
