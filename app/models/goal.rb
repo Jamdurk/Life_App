f98b2f34ab8fc2405c4d2050253824974f46abf5
James <james.savona@outlook.com>
2025-12-17 12:23:06 +1100

    Added character length restriction to goal model

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,6 +1,6 @@
 class Goal < ApplicationRecord
     # validations ensure the data must be present. No length limit here similar to the daily entries as these can be long.
     # Also no dates, as our goals may be long term or not having a specific start and end date.
-    validates :goal_content, presence: true
+    validates :goal_content, presence: true, length: { maximum: 150 }
     validates :goal_start, presence: true
 end
