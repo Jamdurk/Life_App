25046158e3ca9886ac410d4157d5413dbe8e9b2b
James <james.savona@outlook.com>
2025-12-22 18:56:51 +1100

    Added mood validation requiring the submission of 1-3

--- app/models/daily_entry.rb
+++ app/models/daily_entry.rb
@@ -2,5 +2,5 @@ class DailyEntry < ApplicationRecord
     # validations ensure the data must be present. No limit is given since the entries will be quite long most likely.
     validates :date, presence: true 
     validates :day_content, presence: true
-    validates :mood, presence: true
+    validates :mood, presence: true, inclusion: { in: [1, 2, 3] }
 end
