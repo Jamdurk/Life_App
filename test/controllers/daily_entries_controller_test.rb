bd7bc97d2173a9579943c9fedf7bfaab277e277c
James <james.savona@outlook.com>
2026-01-19 21:46:27 +1100

    Overhauled and fixed up all test suites with AI, specifcally making the error recognition tests less brittle

--- test/controllers/daily_entries_controller_test.rb
+++ test/controllers/daily_entries_controller_test.rb
@@ -1,7 +1,6 @@
 require "test_helper"
 
 class DailyEntriesControllerTest < ActionDispatch::IntegrationTest
-
   test "should get home" do
     get root_path
     assert_response :success
@@ -11,22 +10,34 @@ class DailyEntriesControllerTest < ActionDispatch::IntegrationTest
     assert_difference("DailyEntry.count") do
       post daily_entries_path, params: { daily_entry: { day_content: "Test day content", mood: 1 } }
     end
-    assert_redirected_to daily_entries_path
+
+    assert_redirected_to root_path
   end
 
   test "should not create daily entry without day content" do
     assert_no_difference("DailyEntry.count") do
-      post daily_entries_path, params: { daily_entry: { day_content: "",  mood: 1} }
-      assert_select "div#error_explanation",  "The form contains 1 error • Day content can't be blank"
+      post daily_entries_path, params: { daily_entry: { day_content: "", mood: 1 } }
     end
+
+    assert_response :unprocessable_entity
+    assert_select "div#error_explanation li", /Day content can't be blank/
   end
 
   test "should not create daily entry without a mood" do
     assert_no_difference("DailyEntry.count") do
-      post daily_entries_path, params: { daily_entry: { day_content: "Hi",  mood: nil } }
-      assert_select "div#error_explanation",  "The form contains 2 errors • Mood can't be blank • Mood is not included in the list"
+      post daily_entries_path, params: { daily_entry: { day_content: "Hi", mood: nil } }
     end
-  end
-    
 
+    assert_response :unprocessable_entity
+    assert_select "div#error_explanation li", /Mood can't be blank/
+  end
+
+  test "should not create daily entry if mood exceeds 3" do
+    assert_no_difference("DailyEntry.count") do
+      post daily_entries_path, params: { daily_entry: { day_content: "This is a test", mood: 4 } }
+    end
+
+    assert_response :unprocessable_entity
+    assert_select "div#error_explanation li", /Mood is not included in the list/
+  end
 end
--- test/controllers/goals_controller_test.rb
+++ test/controllers/goals_controller_test.rb
@@ -1,7 +1,6 @@
 require "test_helper"
 
 class GoalsControllerTest < ActionDispatch::IntegrationTest
-
   test "should get goal page" do
     get goals_path
     assert_response :success
@@ -9,37 +8,52 @@ class GoalsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create goal" do
     assert_difference("Goal.count") do
-      post goals_path, params: { goal: { goal_content: "Hello, im a test!", goal_start: Date.current, goal_end: Date.current } }
+      post goals_path, params: {
+        goal: { goal_content: "Hello, I'm a test!", goal_start: Date.current, goal_end: Date.current }
+      }
     end
+
     assert_redirected_to goals_path
   end
 
   test "should not create goal without goal content" do
     assert_no_difference("Goal.count") do
-      post goals_path, params: { goal: { goal_content: "",  goal_start: Date.current, goal_end: Date.current } }
-      assert_select "div#error_explanation",  "The form contains 1 error • Goal content can't be blank"
+      post goals_path, params: {
+        goal: { goal_content: "", goal_start: Date.current, goal_end: Date.current }
+      }
     end
+
+    assert_response :unprocessable_entity
+    assert_select "div#error_explanation li", /Goal content can't be blank/
   end
 
   test "should not create goal without a start date" do
     assert_no_difference("Goal.count") do
-      post goals_path, params: { goal: { goal_content: "Hi im still a test....",  goal_start: nil, goal_end: Date.current } }
-      assert_select "div#error_explanation",  "The form contains 1 error • Goal start can't be blank"
+      post goals_path, params: {
+        goal: { goal_content: "Hi, I'm still a test....", goal_start: nil, goal_end: Date.current }
+      }
     end
+
+    assert_response :unprocessable_entity
+    assert_select "div#error_explanation li", /Goal start can't be blank/
   end
 
   test "can create goal with no end date" do
     assert_difference("Goal.count") do
-      post goals_path, params: { goal: { goal_content: "Really? Still testing?", goal_start: Date.current, goal_end: nil } }
+      post goals_path, params: {
+        goal: { goal_content: "Really? Still testing?", goal_start: Date.current, goal_end: nil }
+      }
     end
   end
 
   test "goal content should not exceed 150 characters" do
     assert_no_difference("Goal.count") do
-      post goals_path, params: { goal: { goal_content: "Test" * 150, goal_start: Date.current, goal_end: Date.current } }
-      assert_select "div#error_explanation",  "The form contains 1 error • Goal content is too long (maximum is 150 characters)"
+      post goals_path, params: {
+        goal: { goal_content: "Test" * 151, goal_start: Date.current, goal_end: Date.current }
+      }
     end
+
+    assert_response :unprocessable_entity
+    assert_select "div#error_explanation li", /Goal content is too long/
   end
-
-
 end
