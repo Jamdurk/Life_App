39ff3415f1d6a322d749a85ec8f3177a626cc558
James <james.savona@outlook.com>
2026-02-10 21:38:45 +1100

    Added show to resources for all three

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,9 @@ Rails.application.routes.draw do
   get "/goals", to: "goals#goals_page"
   get "/hobbies", to: "hobbies#hobbies_page"
 
-  resources :daily_entries, only: [:edit, :update,  :create, :destroy]
-  resources :goals, only: [:edit, :update,  :create, :destroy]
-  resources :hobbies, only: [:edit, :update,  :create, :destroy]
+  resources :daily_entries, only: [:edit, :update, :show,  :create, :destroy]
+  resources :goals, only: [:edit, :update, :show, :create, :destroy]
+  resources :hobbies, only: [:edit, :update, :show, :create, :destroy]
 
   get "up" => "rails/health#show", as: :rails_health_check
 end
